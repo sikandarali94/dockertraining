83558d6f083ec68968f75427331d425d7bf4d2ce
Sikandar Ali <sikandarali94@hotmail.com>
2020-07-31 08:24:26 +1000

    finished sec5 lec54 networking with docker compose

--- index.js
+++ index.js
@@ -2,7 +2,15 @@ const express = require('express');
 const redis = require('redis');
 
 const app = express();
-const client = redis.createClient();
+const client = redis.createClient({
+    /* We do not provide a connection url when connecting a container to another deployed by Docker compose. Instead, we
+    just provide the name of the service/container. When the Redis library makes a request to this host name, Docker
+    will look at it and automatically understand which service/container we want to connect to because we are providing
+    the name of the service/container. */
+    host: 'redis-server',
+    /* While this is not required, we provided a port for completion sake, as this is the default Redis port. */
+    port: 6379
+});
 client.set('visits', 0);
 
 app.get('/', (req, res) => {
