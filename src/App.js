979b764ecc928344c8d38d20b68bcf96bf12faab
Sikandar Ali <sikandarali94@hotmail.com>
2020-08-03 23:00:47 +1000

    finished sec6 lec79 do we need copy?

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to. Hi there.
+          Edit <code>src/App.js</code> and save to here. Hi over there.
         </p>
         <a
           className="App-link"
