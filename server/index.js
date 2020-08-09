1a8673ec73e30267942b9e907f1af6b561cc2bf6
Sikandar Ali <sikandarali94@hotmail.com>
2020-08-09 19:46:25 +1000

    finished sec9 lec131 the worker and client services

--- server/index.js
+++ server/index.js
@@ -21,10 +21,11 @@ const pgClient = new Pool({
     port: keys.pgPort
 });
 
-pgClient.on('error', () => console.log('Lost PG connection'));
-
-pgClient.query('CREATE TABLE IF NOT EXISTS values (number INT)')
-    .catch(err => console.log(err));
+pgClient.on('connect', () => {
+    pgClient
+        .query('CREATE TABLE IF NOT EXISTS values (number INT)')
+        .catch(err => console.log(err));
+});
 
 // Redis Client Setup
 const redis = require('redis');
